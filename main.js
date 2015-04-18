58e8017369229ca06004f245bdd978f0dc4b474d
FangedTerror802 <alexr23@outlook.com>
2015-04-17 20:46:37 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+var myName = "FangedTerror";
+	var white = [277, 277, 277];
+	var purple = [280, 50, 60];
+	var letterColors = [white, purple];
+	drawName(myName, letterColors);
+	if(10 < 3) {
+	bubbleShape = 'square';
+	} else {
+	bubbleShape = 'circle';
+	}
+	bounceBubbles();
